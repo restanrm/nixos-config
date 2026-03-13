47697206ed6dc796cc3e6a55b49b0710f6d62b36
Adrien Raffin <adrien.raffin@sekoia.io>
2026-03-13 21:00:48 +0100

    deploy nvim configuration with nix

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -8,4 +8,11 @@
     EDITOR = "nvim";
     VISUAL = "nvim";
   };
+
+  # Déploie la configuration nvim de façon déclarative
+  # lazy-lock.json n'est pas géré ici car lazy.nvim doit pouvoir y écrire
+  xdg.configFile."nvim/init.lua".source = ./assets/init.lua;
+  xdg.configFile."nvim/lua".source = ./assets/lua;
+  xdg.configFile."nvim/lazyvim.json".source = ./assets/lazyvim.json;
+  xdg.configFile."nvim/stylua.toml".source = ./assets/stylua.toml;
 }
