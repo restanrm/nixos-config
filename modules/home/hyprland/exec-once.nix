3b3c7eaa64d1003a2eaa8af30edd999d7323883c
Adrien Raffin <adrien.raffin@sekoia.io>
2026-03-24 08:37:07 +0100

    fix: remove double wl-paste daemon

--- modules/home/hyprland/exec-once.nix
+++ modules/home/hyprland/exec-once.nix
@@ -1,8 +1,8 @@
 {...}: {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "wl-paste --type text --watch cliphist store" # saves text
-      "wl-paste --type image --watch cliphist store" # saves images
+      # "wl-paste --type text --watch cliphist store" # saves text
+      # "wl-paste --type image --watch cliphist store" # saves images
       # "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
       # "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
       # "systemctl --user start hyprpolkitagent"
