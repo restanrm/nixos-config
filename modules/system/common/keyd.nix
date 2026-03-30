805b38628a1f7c2cb154182bca1293824c269830
Adrien Raffin <adrien.raffin@sekoia.io>
2026-03-30 19:00:27 +0200

    feat(keyd): overload capslock to shift and escape

--- modules/system/common/keyd.nix
+++ modules/system/common/keyd.nix
@@ -6,7 +6,7 @@
         ids = ["*"];
         settings = {
           main = {
-            capslock = "overload(capslock, esc)";
+            capslock = "overload(shift, esc)";
           };
           alt = {
             h = "left";
