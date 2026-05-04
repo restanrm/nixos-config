f4914635069197bbdd78cc0e17fd3973325921e7
Adrien Raffin <adrien.raffin@sekoia.io>
2026-05-04 15:27:43 +0200

    feat: try to fix bluetooth modules for phone

--- modules/system/common/audio.nix
+++ modules/system/common/audio.nix
@@ -6,15 +6,29 @@
     openFirewall = true;
   };
 
+  services.blueman.enable = true;
+
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa.enable = true;
 
     # Configuration directe du module RAOP comme sur votre Arch
-    extraConfig.pipewire."99-airplay" = {
-      "context.modules" = [
-        {name = "libpipewire-module-raop-discover";}
-      ];
+    extraConfig.pipewire = {
+      "99-airplay" = {
+        "context.modules" = [
+          {name = "libpipewire-module-raop-discover";}
+        ];
+      };
+      # try to fix sound issues with bluetooth and phone
+      "10-bluez" = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = ["a2dp_sink" "a2dp_source" "hfp_hf" "hfp_ag"];
+        };
+      };
     };
   };
 }
