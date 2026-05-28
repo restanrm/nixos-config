2d6c8fbea7dd9f6249eb78e252b6d7e372bc5d82
Adrien Raffin <adrien.raffin@sekoia.io>
2026-05-28 15:05:37 +0200

    fix(fonts): install emoji fonts

--- modules/system/common/fonts.nix
+++ modules/system/common/fonts.nix
@@ -2,5 +2,9 @@
   fonts.packages = with pkgs; [
     nerd-fonts.noto
     nerd-fonts.fira-code
+    noto-fonts
+    noto-fonts-color-emoji
   ];
+
+  fonts.fontconfig.defaultFonts.emoji = ["Noto Color Emoji"];
 }
